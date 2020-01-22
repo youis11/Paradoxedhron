07d3dfc27c8b3b67e94d09232ce4ee19f8a13677
OriolCS2 <36154523+OriolCS2@users.noreply.github.com>
2020-01-22 10:37:33 +0100

    arfesf

---
+++ Paradox/Assets/RotateGear.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateGear : MonoBehaviour
+{
+    public bool left = true;
+    public float velocity = 5;
+    float angle = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, angle += (left) ? velocity * Time.deltaTime : -velocity * Time.deltaTime);
+    }
+}
